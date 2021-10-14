f2a208393946479e71638beece36ac7cf890eed8
PamPaula <pamela.dpaulas@gmail.com>
2021-10-14 10:33:43 -0300

    criação de botão yes & no

---
+++ YesOrNo/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { DisableControlModule } from './shared/directives/disable-control/disable-control.module';
+import { YesNoButtonGroupComponent } from './shared/components/yes-no-button-group/yes-no-button-group.component';
+import { YesNoButtonGroupModule } from './shared/components/yes-no-button-group/yes-no-button-group.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    YesNoButtonGroupModule,
+    ReactiveFormsModule,
+    FormsModule,
+    DisableControlModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
