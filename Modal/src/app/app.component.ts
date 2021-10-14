d8f10d446321d96d22cd04114cbca9f28b549bd7
PamPaula <pamela.dpaulas@gmail.com>
2021-10-14 19:10:42 -0300

    criando modal acessivel

--- Modal/src/app/app.component.ts
+++ Modal/src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { ModalService } from './shared/components/modal/services/modal.service';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { ModalRef } from './shared/components/modal/models/modal-ref';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @ViewChild('modal') public modalTemplateRef: TemplateRef<any>;
   title = 'a11y-p2';
+  public firstName = 'Pamela';
+  public modalRef: ModalRef;
+  public info = false;
+  public form: FormGroup;
+
+  constructor(
+    private modalService: ModalService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  public ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      firstName: ['Pamela', Validators.required],
+      surname: ['', Validators.required],
+      age: ['', Validators.required],
+      info: [false]
+    })
+  }
+
+  public show(){
+    this.modalRef = this.modalService.open({
+      templateRef: this.modalTemplateRef,
+      title: 'User Details'
+    });
+  }
+
+  public submit(): void{
+    if (this.form.invalid) {
+      return;
+    }
+    console.log(this.form.value);
+    this.modalRef.close();
+  }
 }
